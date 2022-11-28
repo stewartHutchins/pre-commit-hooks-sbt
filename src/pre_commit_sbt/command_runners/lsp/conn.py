2a800722966b0b6d06ef25dfed9bcbfb7a0ccfab
Stewart Hutchins <stewartmhutchins@gmail.com>
2022-11-28 16:32:48 +0000

    Add documentation for connect_to_sbt_server.

--- src/pre_commit_sbt/command_runners/lsp/conn.py
+++ src/pre_commit_sbt/command_runners/lsp/conn.py
@@ -3,6 +3,11 @@ from pathlib import Path
 
 
 def connect_to_sbt_server(socket_file: Path) -> socket.socket:
+    """
+    Create a connection to a unix socket
+    :param socket_file: The path to the socket
+    :return: A socket connection
+    """
     sbt_connection = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     sbt_connection.connect(str(socket_file))
     return sbt_connection
