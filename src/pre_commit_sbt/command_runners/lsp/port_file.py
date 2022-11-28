b0b8e2a093ca1aacac8409f0656aa150b9e41db6
Stewart Hutchins <stewartmhutchins@gmail.com>
2022-11-28 15:25:40 +0000

    Add documentation for port_path

--- src/pre_commit_sbt/command_runners/lsp/port_file.py
+++ src/pre_commit_sbt/command_runners/lsp/port_file.py
@@ -7,6 +7,11 @@ _ACTIVE_JSON_PATH = "project/target/active.json"
 
 
 def port_path(root_dir: Path) -> Path:
+    """
+    Get the location of a port file, given the directory an SBT server is running in
+    :param root_dir: The root directory of an SBT server
+    :return: The path to the port file
+    """
     return root_dir.joinpath(_ACTIVE_JSON_PATH)
 
 
