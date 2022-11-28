e65b76ed945c787ab736dc14f1c98bca29d03ef7
Stewart Hutchins <stewartmhutchins@gmail.com>
2022-11-28 15:31:38 +0000

    Add documentation for is_server_running.

--- src/pre_commit_sbt/command_runners/lsp/server.py
+++ src/pre_commit_sbt/command_runners/lsp/server.py
@@ -4,4 +4,9 @@ from pre_commit_sbt.command_runners.lsp.port_file import port_path
 
 
 def is_server_running(root_dir: Path) -> bool:
+    """
+    Determine whether the server is running, based on the presence or lack there of an SBT port file
+    :param root_dir: The root directory of the project
+    :return: True if SBT server is running in this directory, else False
+    """
     return port_path(root_dir).exists()
