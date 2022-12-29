86eb84b0218f156d23fdf0f0b4705457305fcd89
Stewart Hutchins <stewartmhutchins@gmail.com>
2022-12-29 21:06:35 +0000

    Revert "Add utility to connect to sbt server socket."
    This reverts commit 919aa53e43f66d4bee54a16ffbd6176faa897040.

--- src/pre_commit_sbt/lsp/conn.py
+++
@@ -1,8 +0,0 @@
-import socket
-from pathlib import Path
-
-
-def connect_to_sbt_server(socket_file: Path) -> socket.socket:
-    sbt_connection = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    sbt_connection.connect(str(socket_file))
-    return sbt_connection
