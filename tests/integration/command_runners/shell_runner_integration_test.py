b5ab602508cd69348d9ac3228a42367a2c72d4fe
Stewart Hutchins <stewartmhutchins@gmail.com>
2022-12-29 22:20:59 +0000

    Revert "Add module for discovering and reading port file. Add fixture for sbt project with server, without server, and both."
    This reverts commit dc58278641e539f9d884d46fbcb549ae51aa3949.

--- tests/integration/command_runners/shell_runner_integration_test.py
+++ tests/integration/command_runners/shell_runner_integration_test.py
@@ -9,26 +9,26 @@ from pre_commit_sbt.err.failed_command_error import FailedCommandError
 
 
 @pytest.mark.asyncio
-async def test_run_via_commandline_runs(sbt_project_without_server: Path) -> None:
+async def test_run_via_commandline_runs_sbt_valid(sbt_project: Path) -> None:
     """Run a sbt command via the commandline successfully"""
     # arrange
-    add_touch_command_to_sbt(sbt_project_without_server, "touch")
+    add_touch_command_to_sbt(sbt_project, "touch")
     file_to_create = "sample_file.txt"
 
     # act
-    await run_via_commandline(f'touch "{file_to_create}"', sbt_project_without_server)
+    await run_via_commandline(f'touch "{file_to_create}"', sbt_project)
 
     # assert
-    expected_file = sbt_project_without_server.joinpath(file_to_create)
+    expected_file = sbt_project.joinpath(file_to_create)
     assert expected_file.exists()
 
 
 @pytest.mark.asyncio
-async def test_run_via_commandline_runs_invalid_command(sbt_project_without_server: Path) -> None:
+async def test_run_via_commandline_runs_invalid_command(sbt_project: Path) -> None:
     """Running an invalid sbt command via the commandline should raise an error"""
 
     # act & assert
     with pytest.raises(FailedCommandError) as actual:
-        await run_via_commandline("non_existing_command", sbt_project_without_server)
+        await run_via_commandline("non_existing_command", sbt_project)
 
     assert actual.value.args[0] == COMMAND_FAILED
