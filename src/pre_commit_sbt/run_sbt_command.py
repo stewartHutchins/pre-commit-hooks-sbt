001481a383c109c14e403275dbdd2ca86d4cced9
Stewart Hutchins <stewartmhutchins@gmail.com>
2022-12-29 21:01:22 +0000

    Revert "Alter run_sbt_command to utilise the new command runner."
    This reverts commit 9d19c17b3b985f45812095a14bd3e026a5debe6e.

--- src/pre_commit_sbt/run_sbt_command.py
+++ src/pre_commit_sbt/run_sbt_command.py
@@ -5,12 +5,7 @@ from pathlib import Path
 from pre_commit_sbt.args.parse_args import arg_parser
 from pre_commit_sbt.args.parse_args import sbt_command
 from pre_commit_sbt.args.parse_args import timeout
-from pre_commit_sbt.command_runners.lsp_runner import run_via_lsp
 from pre_commit_sbt.command_runners.shell_runner import run_via_commandline
-from pre_commit_sbt.lsp.conn import connect_to_sbt_server
-from pre_commit_sbt.lsp.port_file import connection_details
-from pre_commit_sbt.lsp.port_file import port_path
-from pre_commit_sbt.lsp.server import is_server_running
 
 
 async def main_async(args: list[str] | None = None, cwd: Path = Path(".")) -> int:
@@ -23,11 +18,7 @@ async def main_async(args: list[str] | None = None, cwd: Path = Path(".")) -> in
 
 
 async def _run_sbt_command(command: str, cwd: Path) -> None:
-    if is_server_running(cwd):
-        with (open(port_path(cwd), encoding="UTF-8") as fp, connect_to_sbt_server(connection_details(fp)) as conn):
-            await run_via_lsp(command, conn)
-    else:
-        await run_via_commandline(command, cwd)
+    await run_via_commandline(command, cwd)
 
 
 def main(args: list[str] | None = None) -> int:
